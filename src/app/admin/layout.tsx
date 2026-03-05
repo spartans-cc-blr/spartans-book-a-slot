8a989c5368fa57c8ce0ee00b4ad72ec56fcfea81
spartans-cc <spartans.cricket.blr@gmail.com>
2026-03-05 21:11:34 +0530

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,7 +7,7 @@ export default async function AdminLayout({ children }: { children: React.ReactN
   const session = await getServerSession(authOptions)
 
   if (!session) {
-    redirect('/admin/login')
+    redirect('/login')
   }
 
   return (
