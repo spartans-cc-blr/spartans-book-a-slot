0af08f3023c084a80fba609d2004b0b60d93009b
Spartans CC Bengaluru <spartans.cricket.blr@gmail.com>
2026-06-19 09:33:33 +0530

    zoom out and in to see the details of the 2024 Spartans Club Collection.

--- src/app/dugout/kit-room/page.tsx
+++ src/app/dugout/kit-room/page.tsx
@@ -54,7 +54,7 @@ export default async function KitRoomPage() {
               width={1200}
               height={220}
               className="w-full h-full object-cover"
-              style={{ objectPosition: 'center 30%' }}
+              style={{ objectPosition: 'center 20%' }}
               priority
             />
           </div>
