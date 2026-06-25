28e9027c9c17eff98c1df9198d900f1e9c9da7d1
Spartans CC Bengaluru <spartans.cricket.blr@gmail.com>
2026-06-25 08:11:19 +0530

    fixing the service worker to cache only the necessary files and remove unnecessary files from the cache.

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ const OFFLINE_URL = '/offline'
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache =>
-      cache.addAll(['/', '/fixtures', '/offline', '/icons/icon-192.png'])
+      cache.addAll(['/', '/fixtures'])
     )
   )
   self.skipWaiting()
